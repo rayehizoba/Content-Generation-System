f103bed840dfd1190d32c0dddb3fd7a00f4b8215
Raymond Ehizoba <rayehizoba@gmail.com>
2021-04-27 23:45:23 +0000

    wip: theme

--- theme/tailwind.config.js
+++ theme/tailwind.config.js
@@ -5,7 +5,26 @@ module.exports = {
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        '#2B2B2B': '#2B2B2B',
+        '#FAFAFA': '#FAFAFA',
+        '#888686': '#888686',
+        '#2E4ADB': '#2E4ADB',
+        '#3551E6': '#3551E6',
+        '#3F5AEB': '#3F5AEB',
+        '#4B65F5': '#4B65F5',
+        '#4B93F5': '#4B93F5',
+        '#707070': '#707070',
+        '#D5D5D5': '#D5D5D5',
+        '#A7CCFF': '#A7CCFF',
+        '#2EC98D': '#2EC98D',
+        '#2EC9C3': '#2EC9C3',
+      },
+      fontFamily: {
+        sans: ['Nunito']
+      }
+    },
   },
   variants: {
     extend: {},
