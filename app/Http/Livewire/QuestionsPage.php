48090e5d64bc3e29a8dd513db4c83d1c59fe10fc
Raymond Ehizoba <rayehizoba@gmail.com>
2021-04-30 15:41:11 +0000

    questions page, case insensitive search

--- app/Http/Livewire/QuestionsPage.php
+++ app/Http/Livewire/QuestionsPage.php
@@ -21,7 +21,7 @@ class QuestionsPage extends Component
     public function render()
     {
         $query = Question::query();
-        $query->where('label', 'like', '%'.$this->search.'%');
+        $query->whereRaw("UPPER('label') LIKE '%'". strtoupper($this->search)."'%'");
         $query->when($this->status, function ($q) {
             return $q->where('status_id', $this->status);
         });
