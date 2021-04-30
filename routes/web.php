a09fd828d7672336725eef4a06d6cd60c813e14d
Raymond Ehizoba <rayehizoba@gmail.com>
2021-04-30 14:47:47 +0000

    add procfile

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Livewire\QuestionsPage;
+use App\Http\Livewire\CreateQuestionPage;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,3 +20,4 @@ Route::get('/', function () {
 });
 
 Route::get('/questions', QuestionsPage::class);
+Route::get('/create-question', CreateQuestionPage::class);
